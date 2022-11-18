47e08052ebbfd3455d45eba1ba2c1fabac984c45
VikumRavinayake <vikumravinayake2@gmail.com>
2022-11-18 23:28:41 +0530

    api-posts-all-post-populate

--- controllers/postControllers/post.all-viewController.js
+++ controllers/postControllers/post.all-viewController.js
@@ -1,4 +1,5 @@
 const Post = require('../../models/postModel')
+const User = require('../../models/userModel')
 
 const postAllViewHandler = async (req, res) => {
     const userEmail = res.locals.mail
@@ -6,8 +7,9 @@ const postAllViewHandler = async (req, res) => {
 
     try {
         const posts = await Post.find()
+        const user = await User.find({email:userEmail})
         // console.log(posts);
-        res.status(200).json({success:true, message:'All posts are received', posts:posts}) 
+        res.status(200).json({success:true, message:'All posts are received', posts:posts, firstName:user.firstname, lastName:user.lastname }) 
     } catch (error) {
         res.status(400).json({success:false, message:'All post loading err'})
     }
