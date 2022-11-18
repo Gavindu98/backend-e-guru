ee3a20b22911e38472af964bac283a414a35add1
Gavindu98 <rathnayakagavindu98@gmail.com>
2022-11-19 00:26:07 +0530

    delete controller

--- controllers/postControllers/post.deleteController.js
+++ controllers/postControllers/post.deleteController.js
@@ -6,29 +6,29 @@ const fileOpt = async (location) => {
     try {
         await fsPromises.rm(location)
     } catch (error) {
-        console.log(err);
+        console.log(error);
     }
 }
 
 const postDeleteHandler = async (req, res) => {
     const { postID } = req.body
     const authorizedEmail = res.locals.mail
-    if(!postID) return res.status(400).json({success:false, message:'Unable to identify the post to delete'})
+    if (!postID) return res.status(400).json({ success: false, message: 'Unable to identify the post to delete' })
 
     try {
         const post = await Post.findById(postID)
         const pathLocation = post.filePath
-        if(!post) return res.status(404).json({success:false, message:'Post is not found'})
-        if(post.creator.email != authorizedEmail) return res.status(401).json({success:false, message:'Unauthorized try'})
+        if (!post) return res.status(404).json({ success: false, message: 'Post is not found' })
+        if (post.creator.email != authorizedEmail) return res.status(401).json({ success: false, message: 'Unauthorized try' })
         // console.log(post);
 
         // deletion of document
-        await Post.deleteOne({_id: postID})
+        await Post.deleteOne({ _id: postID })
         fileOpt(pathLocation)
-        res.status(200).json({success:true, message: 'Post successfully deleted'})
+        res.status(200).json({ success: true, message: 'Post successfully deleted' })
 
     } catch (error) {
-        res.status(409).json({success:false, message:'Deletion is failed'})
+        res.status(409).json({ success: false, message: 'Deletion is failed' })
     }
 }
 
