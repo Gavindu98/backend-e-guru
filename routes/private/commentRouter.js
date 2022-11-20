c1b5f422e80dcb1d7ba191482b38700e0a74d509
Gavindu98 <rathnayakagavindu98@gmail.com>
2022-11-20 17:41:24 +0530

    get all comment api changes

--- routes/private/commentRouter.js
+++ routes/private/commentRouter.js
@@ -6,8 +6,8 @@ const verifyAccessToken = require('../../middleware/verifyJWT')
 const verifyRoles = require('../../middleware/verifyRole')
 const USER_ROLES = require('../../config/userRoles')
 
-router.route('/comment-create').post(verifyAccessToken, verifyRoles(USER_ROLES.STUDENT, USER_ROLES.TUTOR), commentCreateHandler )
-router.route('/comment-all-view').get(verifyAccessToken, verifyRoles(USER_ROLES.STUDENT, USER_ROLES.TUTOR), commentAllViewHandler )
+router.route('/comment-create').post(verifyAccessToken, verifyRoles(USER_ROLES.STUDENT, USER_ROLES.TUTOR), commentCreateHandler)
+router.route('/comment-all-view').post(verifyAccessToken, verifyRoles(USER_ROLES.STUDENT, USER_ROLES.TUTOR), commentAllViewHandler)
 
 
 module.exports = router
