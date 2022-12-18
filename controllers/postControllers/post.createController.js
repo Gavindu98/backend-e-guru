35d2f391de86ee983a1569372bbfb40237c567ad
Gavindu98 <rathnayakagavindu98@gmail.com>
2022-12-18 15:22:26 +0530

    post create path

--- controllers/postControllers/post.createController.js
+++ controllers/postControllers/post.createController.js
@@ -5,23 +5,24 @@ const { google } = require('googleapis')
 const path = require('path')
 
 const postCreateHandler = async (req, res) => {
-    const {title, description } = req.body
-    if(!title || !description ) return res.status(400).json({success: false, message: 'missing body value'})
+    const { title, description } = req.body
+    if (!title || !description) return res.status(400).json({ success: false, message: 'missing body value' })
 
     // extract creator details 
     const creatorMail = res.locals.mail // passing data from one to next middleware
-    if(!creatorMail) return res.status(410)
+    if (!creatorMail) return res.status(410)
 
     // extract file details
-    const { filename } = req.file
-    if(!filename) return res.status(410)
+    const { filename, path } = req.file
+    console.log("path==>", path)
+    if (!filename) return res.status(410)
     // console.log(filename);
-    // const localStoreDestination = path.join(process.env.VIKUM_DIR, `storage/images/${filename}`, )
-    const localStoreDestination = path.join(__dirname,`../../storage/images/${filename}`)
-    // console.log(localStoreDestination);
+    const localStoreDestination = path.join(`${path}/${filename}`)
+    // const localStoreDestination = path.join(__dirname, `../../storage/images/${filename}`)
+    console.log("localStoreDestination", localStoreDestination);
     // return res.status(200).json({success: true})
     try {
-        const user = await User.findOne({email:creatorMail})
+        const user = await User.findOne({ email: creatorMail })
         // save post to db
         const newPost = await Post.create({
             "title": title,
@@ -35,16 +36,18 @@ const postCreateHandler = async (req, res) => {
         })
 
         const post = await Post.findById(newPost._id)
-        
+
         // console.log(user.firstname);
         // console.log(newPost);
         res.status(201).json({
-            success:true, 
+            success: true,
             message: 'Post has been created',
-            post : post
+            post: post,
+            path: path,
+            localStoreDestination: localStoreDestination
         })
     } catch (error) {
-        res.status(401).json({success:false, message: 'Post creation is failed'})
+        res.status(401).json({ success: false, message: 'Post creation is failed' })
     }
 }
 
