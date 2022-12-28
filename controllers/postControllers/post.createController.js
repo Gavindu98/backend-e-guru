4f83efd170ac9fd0f9b4a48d3778bd1a9746c0d3
VikumRavinayake <vikumravinayake2@gmail.com>
2022-12-28 23:42:25 +0530

    post create controller change

--- controllers/postControllers/post.createController.js
+++ controllers/postControllers/post.createController.js
@@ -26,7 +26,9 @@ const postCreateHandler = async (req, res) => {
     // return res.status(200).json({success: true})
     try {
         const imageUrl = await uploadImage(localPath)
+        console.log('image url',imageUrl);
         const user = await User.findOne({ email: creatorMail })
+        console.log('user',user);
         // save post to db
         const newPost = await Post.create({
             "title": title,
@@ -38,19 +40,19 @@ const postCreateHandler = async (req, res) => {
             "filePath": imageUrl
         })
 
-        const post = await Post.findById(newPost._id)
-
+        // const post = await Post.findById(newPost._id)
+        
         // console.log(user.firstname);
-        // console.log(newPost);
+        console.log('new ', newPost);
         res.status(201).json({
             success: true,
             message: 'Post has been created',
-            post: post,
-            path: path,
-            localStoreDestination: localStoreDestination
+            post: newPost,
+            // path: path,
+            // localStoreDestination: localStoreDestination
         })
     } catch (error) {
-        res.status(401).json({ success: false, message: 'Post creation is failed' })
+        res.status(500).json({ success: false, message: 'Post creation is failed' })
     }
 }
 
